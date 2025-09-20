117f5a39105d3d7e5a25440cc30e33dcbb7f69aa
Daham Dissanayake <induwaradaham99@gmail.com>
2025-09-20 23:33:56 +0530

    prepared for the deployment

--- Backend/src/sever.js
+++ Backend/src/sever.js
@@ -5,6 +5,7 @@ import { ConnectDB } from "./config/db.js"; // Function to connect to MongoDB
 import dotenv from "dotenv"; // Load environment variables
 import rateLimiter from "./middieware/rateLimiter.js"; // Middleware to limit requests
 import cors from "cors"; // Middleware for Cross-Origin Resource Sharing
+import path from "path";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -12,18 +13,20 @@ dotenv.config();
 // Create an Express app
 const app = express();
 const PORT = process.env.PORT || 5001; // Use environment port or default 5001
+const __dirname = path.resolve();
 
 // Connect to MongoDB database
 ConnectDB();
 
 // ----------------- Middleware -----------------
 
-// Enable CORS to allow requests from frontend (http://localhost:5173)
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 // Parse incoming JSON requests and put data in req.body
 app.use(express.json());
@@ -35,6 +38,15 @@ app.use(rateLimiter);
 
 // Routes for handling notes API
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 // ----------------- Start Server -----------------
 
